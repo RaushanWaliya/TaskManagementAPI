d40ba353a77233be14d2aeceb1fadf3f17ab525d
abymemp93 <abymemp93@gmail.com>
2026-02-13 15:04:20 +0530

    Added models for User and task item.

---
+++ TaskManagementAPI/Models/User.cs
@@ -0,0 +1,15 @@
+﻿namespace TaskManagementAPI.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Uername { get; set; }= string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string PasswordHash { get; set; } = string.Empty;
+        public DateTime createdAt { get; set; } = DateTime.UtcNow;
+
+        //Navigation Property
+        public ICollection<TaskItem> Tasks { get; set; } = new List<TaskItem>();
+
+    }
+}
